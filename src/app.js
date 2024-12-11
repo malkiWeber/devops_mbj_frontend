de9492b8da0d8b9b24d800d617800a3ffd5c08af
וובר תמר < @mbjcomp.local>
2024-12-11 15:45:35 +0200

    h

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   const fetchData = () => {
-    fetch('http://localhost:5000/api')
+    fetch('http://34.149.181.240:80/api')
       .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(error => console.error('Error fetching data:', error));
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Malki React App</h1>
-        <button /*onClick={fetchData}*/ className="fetch-button">Fetch backend</button>
+        <button onClick={fetchData} className="fetch-button">Fetch backend</button>
         <p>{message}</p>
       </header>
     </div>
